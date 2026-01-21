ef1b2813fcb3ef3924057140019737da15ebdab2
PJG8806 <jinguy39@gmail.com>
2026-01-21 15:26:34 +0900

    saying -> quote 명칭 변경 및 오류 수정

--- app/models/saying.py
+++
@@ -1,7 +0,0 @@
-from tortoise import models, fields
-
-# 명언 번호, 제목, 내용 추가
-class Saying(models.Model):
-    id = fields.IntField(pk=True)
-    title = fields.CharField(max_length=200)
-    content = fields.TextField()
--- app/schemas/saying.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel, EmailStr, Field
-
-class SayingOut(BaseModel):
-    title = str
-    content: str
