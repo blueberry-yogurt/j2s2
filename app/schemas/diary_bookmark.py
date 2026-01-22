817d6eb3a5ad12296ea35f2c973dc1c2a37d11bd
sangbo <tkdqh57@naver.com>
2026-01-22 12:54:16 +0900

    add to diary_bookmark.

---
+++ app/schemas/diary_bookmark.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class DiaryBookmarkCreate(BaseModel):
+    diary_id: int
+
+
+class DiaryBookmarkResponse(BaseModel):
+    id: int
+    diary_id: int
+    created_at: datetime
+
+    class Config:
+        from_attibutes = True
